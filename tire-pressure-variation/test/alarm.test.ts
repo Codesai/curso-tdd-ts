bd362df3308bd145c337dbccb14cf22b15a15597
freyes <fran@codesai.com>
2024-10-16 12:43:41 +0100

    add Tire Pressure Variation Kata for TDD

--- tire-pressure-variation/test/alarm.test.ts
+++ tire-pressure-variation/test/alarm.test.ts
@@ -1,9 +1,5 @@
-import {Alarm} from "../src/alarm";
-
 describe('Alarm', () => {
   it('fix me', () => {
-    const alarm = new Alarm();
-    alarm.check();
     expect(true).toBe(false);
   });
 });
